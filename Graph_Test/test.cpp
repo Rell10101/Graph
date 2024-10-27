ffc59c05dcabcd97cfe57008e5515cee85dafa11
Rell10101 <anton.samaev321@mail.ru>
2024-10-28 00:24:48 +0900

    test

---
+++ Graph_Test/test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "../ConsoleApplication_Graph/Graph.h"
+using namespace std;
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
+
+// тестирование вставки вершин
+/*
+TEST(TestGraph, TestInsertVertex) {
+	Graph<int> g1(2);
+	EXPECT_EQ(g1.NumberOfVertices(), 0);
+}*/
